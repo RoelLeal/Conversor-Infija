74aeef5cb3a36698b40603e02af4e901e243bce0
roelleal <iroeleal11@gmail.com>
2024-09-24 19:30:32 -0600

    POSTFIJO

--- src/main/java/conversor/conversornotacion/ConversorNotacion.java
+++ src/main/java/conversor/conversornotacion/ConversorNotacion.java
@@ -1,14 +1,61 @@
 package conversor.conversornotacion;
 
+import java.util.ArrayList; // ARREGLO DINAMICO
 import java.util.Stack; //PREFIJA A POSTFIJA
 
 public class ConversorNotacion {
-
+    
+    public static int tope = -1;
+    public static Stack<Character> pila = new Stack<>();
+    public static ArrayList<Character> arreglo = new ArrayList<>();
+    
     public static void main(String[] args) {
-        Stack pila = new Stack<>();
+        
         String expresion = "(6+4)*8(7+4)";
         
-        
-        
+        for(int i = 0; i < expresion.length(); i++) {
+            char valor = expresion.charAt(i);
+            switch(valor) {
+                case '+' -> {
+                    pila.push('+');
+                }
+                case '-' -> {
+                    pila.push('-');
+                }
+                case '/' -> {
+                    pila.push('/');
+                }
+                case '*' -> {
+                    pila.push('*');
+                }
+                case '(' -> {
+                    pila.push('(');
+                }
+                case ')' -> {
+                    vaciarPila();
+                }
+                default -> {
+                    arreglo.add(valor);
+                }
+            }
+        }
+        if(!pila.isEmpty()) {
+           arreglo.add(pila.pop());
+        }
+        for(int i = 0; i < arreglo.size(); i++) {
+            System.out.print(arreglo.get(i));
+        }
+    }
+    
+    public static void vaciarPila() {
+        while(!pila.isEmpty()) {
+            char valor = pila.pop();
+            if(valor == '(') {
+                tope = -1;
+                break;
+            }else {
+                arreglo.add(valor);
+            }
+        }
     }
 }
