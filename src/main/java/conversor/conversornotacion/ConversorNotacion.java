c48d42afda103e877364d6d039897651db6d21fe
roelleal <iroeleal11@gmail.com>
2024-09-24 18:31:10 -0600

    first commit

---
+++ src/main/java/conversor/conversornotacion/ConversorNotacion.java
@@ -0,0 +1,14 @@
+package conversor.conversornotacion;
+
+import java.util.Stack; //PREFIJA A POSTFIJA
+
+public class ConversorNotacion {
+
+    public static void main(String[] args) {
+        Stack pila = new Stack<>();
+        String expresion = "(6+4)*8(7+4)";
+        
+        
+        
+    }
+}
